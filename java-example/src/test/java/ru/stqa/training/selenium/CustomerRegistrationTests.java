37061897603657189f64b76963a80fa40c552133
Alexei Barantsev <barancev@gmail.com>
2016-12-14 17:01:55 +0300

    Сделана заготовка теста на Java

---
+++ java-example/src/test/java/ru/stqa/training/selenium/CustomerRegistrationTests.java
@@ -0,0 +1,11 @@
+package ru.stqa.training.selenium;
+
+import org.junit.Test;
+
+public class CustomerRegistrationTests extends TestBase {
+
+    @Test
+    public void canRegisterCustomer() {
+    }
+
+}
---
+++ java-example/src/test/java/ru/stqa/training/selenium/TestBase.java
@@ -0,0 +1,36 @@
+package ru.stqa.training.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TestBase {
+
+    public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    @Before
+    public void start() {
+        if (tlDriver.get() != null) {
+            driver = tlDriver.get();
+            wait = new WebDriverWait(driver, 10);
+            return;
+        }
+
+        driver = new ChromeDriver();
+        tlDriver.set(driver);
+        wait = new WebDriverWait(driver, 10);
+
+        Runtime.getRuntime().addShutdownHook(
+                new Thread(() -> { driver.quit(); driver = null; }));
+    }
+
+    @After
+    public void stop() {
+        //driver.quit();
+        //driver = null;
+    }
+}
