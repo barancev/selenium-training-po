819702a34729243a85d512e9cd5f0298b5df5567
Alexei Barantsev <barancev@gmail.com>
2016-12-14 17:48:37 +0300

    Реализованы проверки

--- java-example/src/test/java/ru/stqa/training/selenium/CustomerRegistrationTests.java
+++ java-example/src/test/java/ru/stqa/training/selenium/CustomerRegistrationTests.java
@@ -3,11 +3,19 @@ package ru.stqa.training.selenium;
 import com.tngtech.java.junit.dataprovider.DataProvider;
 import com.tngtech.java.junit.dataprovider.DataProviderRunner;
 import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toSet;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(DataProviderRunner.class)
 public class CustomerRegistrationTests extends TestBase {
@@ -15,6 +23,19 @@ public class CustomerRegistrationTests extends TestBase {
     @Test
     @UseDataProvider(value = "validCustomers", location = DataProviders.class)
     public void canRegisterCustomer(Customer customer) {
+        driver.get("http://localhost/litecart/admin");
+        if (driver.findElements(By.id("box-login")).size() > 0) {
+            driver.findElement(By.name("username")).sendKeys("admin");
+            driver.findElement(By.name("password")).sendKeys("admin");
+            driver.findElement(By.name("login")).click();
+            wait.until((WebDriver d) -> d.findElement(By.id("box-apps-menu")));
+        }
+
+        driver.get("http://localhost/litecart/admin/?app=customers&doc=customers");
+        Set<String> oldIds = driver.findElements(By.cssSelector("table.dataTable tr.row")).stream()
+                .map(e -> e.findElements(By.tagName("td")).get(2).getText())
+                .collect(toSet());
+
         driver.get("http://localhost/litecart/en/create_account");
         driver.findElement(By.name("firstname")).sendKeys(customer.getFirstname());
         driver.findElement(By.name("lastname")).sendKeys(customer.getLastname());
@@ -34,6 +55,14 @@ public class CustomerRegistrationTests extends TestBase {
         driver.findElement(By.name("password")).sendKeys(customer.getPassword());
         driver.findElement(By.name("confirmed_password")).sendKeys(customer.getPassword());
         driver.findElement(By.name("create_account")).click();
+
+        driver.get("http://localhost/litecart/admin/?app=customers&doc=customers");
+        Set<String> newIds = driver.findElements(By.cssSelector("table.dataTable tr.row")).stream()
+                .map(e -> e.findElements(By.tagName("td")).get(2).getText())
+                .collect(toSet());
+
+        assertTrue(newIds.containsAll(oldIds));
+        assertTrue(newIds.size() == oldIds.size() + 1);
     }
 
 }
