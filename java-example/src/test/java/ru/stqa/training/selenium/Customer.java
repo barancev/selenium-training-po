8643d79d3f35d588d5054f34f0d3ba9b36e6fa51
Alexei Barantsev <barancev@gmail.com>
2016-12-14 17:21:03 +0300

    Тестовые данные отделены от сценария

---
+++ java-example/src/test/java/ru/stqa/training/selenium/Customer.java
@@ -0,0 +1,72 @@
+package ru.stqa.training.selenium;
+
+public class Customer {
+
+    private String firstname;
+    private String lastname;
+    private String address;
+    private String postcode;
+    private String city;
+    private String country;
+    private String zone;
+    private String email;
+    private String phone;
+    private String password;
+
+    public static Builder newEntity() { return new Customer().new Builder(); }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getZone() {
+        return zone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public class Builder {
+        private Builder() {}
+        public Builder withFirstname(String firstname) { Customer.this.firstname = firstname; return this; }
+        public Builder withLastname(String lastname) { Customer.this.lastname = lastname; return this; }
+        public Builder withAddress(String address) { Customer.this.address = address; return this; }
+        public Builder withPostcode(String postcode) { Customer.this.postcode = postcode; return this; }
+        public Builder withCity(String city) { Customer.this.city = city; return this; }
+        public Builder withCountry(String country) { Customer.this.country = country; return this; }
+        public Builder withZone(String zone) { Customer.this.zone = zone; return this; }
+        public Builder withEmail(String email) { Customer.this.email = email; return this; }
+        public Builder withPhone(String phone) { Customer.this.phone = phone; return this; }
+        public Builder withPassword(String password) { Customer.this.password = password; return this; }
+        public Customer build() {return Customer.this; }
+    }
+}
--- java-example/src/test/java/ru/stqa/training/selenium/CustomerRegistrationTests.java
+++ java-example/src/test/java/ru/stqa/training/selenium/CustomerRegistrationTests.java
@@ -9,23 +9,31 @@ public class CustomerRegistrationTests extends TestBase {
 
     @Test
     public void canRegisterCustomer() {
-        String email = "adam"+System.currentTimeMillis()+"@smith.me";
+        Customer customer = Customer.newEntity()
+                .withFirstname("Adam").withLastname("Smith").withPhone("+0123456789")
+                .withAddress("Hidden Place").withPostcode("12345").withCity("New City")
+                .withCountry("US").withZone("KS")
+                .withEmail("adam"+System.currentTimeMillis()+"@smith.me")
+                .withPassword("qwerty").build();
+
         driver.get("http://localhost/litecart/en/create_account");
-        driver.findElement(By.name("firstname")).sendKeys("Adam");
-        driver.findElement(By.name("lastname")).sendKeys("Smith");
-        driver.findElement(By.name("address1")).sendKeys("Hidden Place");
-        driver.findElement(By.name("postcode")).sendKeys("12345");
-        driver.findElement(By.name("city")).sendKeys("New City");
+        driver.findElement(By.name("firstname")).sendKeys(customer.getFirstname());
+        driver.findElement(By.name("lastname")).sendKeys(customer.getLastname());
+        driver.findElement(By.name("address1")).sendKeys(customer.getAddress());
+        driver.findElement(By.name("postcode")).sendKeys(customer.getPostcode());
+        driver.findElement(By.name("city")).sendKeys(customer.getCity());
 
         driver.findElement(By.cssSelector("[id ^= select2-country_code]")).click();
-        driver.findElement(By.cssSelector(".select2-results__option[id $= US")).click();
-        wait.until((WebDriver d) -> d.findElement(By.cssSelector("select[name=zone_code] option[value=KS]")));
-        new Select(driver.findElement(By.name("zone_code"))).selectByValue("KS");
+        driver.findElement(By.cssSelector(
+                String.format(".select2-results__option[id $= %s", customer.getCountry()))).click();
+        wait.until((WebDriver d) -> d.findElement(
+                By.cssSelector(String.format("select[name=zone_code] option[value=%s]", customer.getZone()))));
+        new Select(driver.findElement(By.name("zone_code"))).selectByValue(customer.getZone());
 
-        driver.findElement(By.name("email")).sendKeys(email);
-        driver.findElement(By.name("phone")).sendKeys("+0123456789");
-        driver.findElement(By.name("password")).sendKeys("qwerty");
-        driver.findElement(By.name("confirmed_password")).sendKeys("qwerty");
+        driver.findElement(By.name("email")).sendKeys(customer.getEmail());
+        driver.findElement(By.name("phone")).sendKeys(customer.getPhone());
+        driver.findElement(By.name("password")).sendKeys(customer.getPassword());
+        driver.findElement(By.name("confirmed_password")).sendKeys(customer.getPassword());
         driver.findElement(By.name("create_account")).click();
     }
 
