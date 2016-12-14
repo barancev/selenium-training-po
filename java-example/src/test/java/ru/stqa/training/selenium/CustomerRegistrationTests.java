2bca10b038c551136a265d2f2612adefa633dde3
Alexei Barantsev <barancev@gmail.com>
2016-12-14 17:54:32 +0300

    Выделены unility methods

--- java-example/src/test/java/ru/stqa/training/selenium/CustomerRegistrationTests.java
+++ java-example/src/test/java/ru/stqa/training/selenium/CustomerRegistrationTests.java
@@ -23,19 +23,17 @@ public class CustomerRegistrationTests extends TestBase {
     @Test
     @UseDataProvider(value = "validCustomers", location = DataProviders.class)
     public void canRegisterCustomer(Customer customer) {
-        driver.get("http://localhost/litecart/admin");
-        if (driver.findElements(By.id("box-login")).size() > 0) {
-            driver.findElement(By.name("username")).sendKeys("admin");
-            driver.findElement(By.name("password")).sendKeys("admin");
-            driver.findElement(By.name("login")).click();
-            wait.until((WebDriver d) -> d.findElement(By.id("box-apps-menu")));
-        }
+        Set<String> oldIds = getCustomerIds();
 
-        driver.get("http://localhost/litecart/admin/?app=customers&doc=customers");
-        Set<String> oldIds = driver.findElements(By.cssSelector("table.dataTable tr.row")).stream()
-                .map(e -> e.findElements(By.tagName("td")).get(2).getText())
-                .collect(toSet());
+        registerNewCustomer(customer);
 
+        Set<String> newIds = getCustomerIds();
+
+        assertTrue(newIds.containsAll(oldIds));
+        assertTrue(newIds.size() == oldIds.size() + 1);
+    }
+
+    private void registerNewCustomer(Customer customer) {
         driver.get("http://localhost/litecart/en/create_account");
         driver.findElement(By.name("firstname")).sendKeys(customer.getFirstname());
         driver.findElement(By.name("lastname")).sendKeys(customer.getLastname());
@@ -55,14 +53,21 @@ public class CustomerRegistrationTests extends TestBase {
         driver.findElement(By.name("password")).sendKeys(customer.getPassword());
         driver.findElement(By.name("confirmed_password")).sendKeys(customer.getPassword());
         driver.findElement(By.name("create_account")).click();
+    }
+
+    private Set<String> getCustomerIds() {
+        driver.get("http://localhost/litecart/admin");
+        if (driver.findElements(By.id("box-login")).size() > 0) {
+            driver.findElement(By.name("username")).sendKeys("admin");
+            driver.findElement(By.name("password")).sendKeys("admin");
+            driver.findElement(By.name("login")).click();
+            wait.until((WebDriver d) -> d.findElement(By.id("box-apps-menu")));
+        }
 
         driver.get("http://localhost/litecart/admin/?app=customers&doc=customers");
-        Set<String> newIds = driver.findElements(By.cssSelector("table.dataTable tr.row")).stream()
+        return driver.findElements(By.cssSelector("table.dataTable tr.row")).stream()
                 .map(e -> e.findElements(By.tagName("td")).get(2).getText())
                 .collect(toSet());
-
-        assertTrue(newIds.containsAll(oldIds));
-        assertTrue(newIds.size() == oldIds.size() + 1);
     }
 
 }
